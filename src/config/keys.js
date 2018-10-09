9f9ceaa31b7c1d6ea4272e7dfc148ac1fb2389dc
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-09 18:09:35 +0900

    add firebase

---
+++ src/config/keys.js
@@ -0,0 +1,8 @@
+export FirebaseConfig = {
+  apiKey: "AIzaSyDMkqYpIYzIKZvswkDBmkbw0T7uJjEcUyM",
+  authDomain: "redux-fb.firebaseapp.com",
+  databaseURL: "https://redux-fb.firebaseio.com",
+  projectId: "redux-fb",
+  storageBucket: "redux-fb.appspot.com",
+  messagingSenderId: "161012003482"
+};
