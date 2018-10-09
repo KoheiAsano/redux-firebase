77c68c625cb4cbb0ec3ee817a7a4ce0352712687
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-09 18:21:26 +0900

    change content of app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-
-import './App.css';
+import ToDoList from "./components/ToDoList";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello World!
+      <div className="container">
+        <ToDoList />
       </div>
     );
   }
